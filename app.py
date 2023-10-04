51c2f2cbaebecaa96d7ca7cddfc918a6a0f6bf86
Lucas Cowe <lucascowe@gmail.com>
2023-10-04 16:46:13 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, jsonify, request
+from deck import Deck
+
+app = Flask(__name__)
+
+deck = Deck()
+deck.shuffle()
+
+player_hand = []
+dealer_hand = []
+
+@app.route('/start', methods=['GET'])
+def start_game():
+    global player_hand, dealer_hand, deck
+
+    player_hand = [deck.draw(), deck.draw()]
+    dealer_hand = [deck.draw(), deck.draw()]
+
+    return jsonify(player=display_hand(player_hand), dealer=display_hand(dealer_hand, hide_first=True))
+
+@app.route('/hit', methods=['GET'])
+def hit():
+    global player_hand
+    player_hand.append(deck.draw())
+    if calculate_total(player_hand) > 21:
+        return jsonify(status="Player busted!", player=display_hand(player_hand))
+    return jsonify(player=display_hand(player_hand))
+
+@app.route('/stand', methods=['GET'])
+def stand():
+    global dealer_hand
+    while calculate_total(dealer_hand) < 17:
+        dealer_hand.append(deck.draw())
+    if calculate_total(dealer_hand) > 21:
+        return jsonify(status="Dealer busted!", dealer=display_hand(dealer_hand))
+    elif calculate_total(dealer_hand) > calculate_total(player_hand):
+        return jsonify(status="Dealer wins!", dealer=display_hand(dealer_hand))
+    elif calculate_total(dealer_hand) < calculate_total(player_hand):
+        return jsonify(status="Player wins!", dealer=display_hand(dealer_hand))
+    else:
+        return jsonify(status="It's a tie!", dealer=display_hand(dealer_hand))
+
+def calculate_total(hand):
+    total = sum(card.get_value() for card in hand)
+    num_aces = sum(1 for card in hand if card.value == 'A')
+    while total > 21 and num_aces:
+        total -= 10
+        num_aces -= 1
+    return total
+
+def display_hand(hand, hide_first=False):
+    if hide_first:
+        return ["hidden"] + [f"{card.value} of {card.suit}" for card in hand[1:]]
+    return [f"{card.value} of {card.suit}" for card in hand]
+
+if __name__ == '__main__':
+    app.run(debug=True)
