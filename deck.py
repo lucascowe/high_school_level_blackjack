572da05de72f37814ddc3a8daf6edb2d0cdb5a1f
Lucas Cowe <lucascowe@gmail.com>
2023-10-04 16:45:42 -0500

    Create deck.py

---
+++ deck.py
@@ -0,0 +1,18 @@
+import random
+from card import Card
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        self.build()
+
+    def build(self):
+        for suit in ["HEART", "DIAMOND", "CLUB", "SPADE"]:
+            for value in ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]:
+                self.cards.append(Card(suit, value))
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def draw(self):
+        return self.cards.pop()
