4ee8e9d0d4a52b47fe1d9900afcb01a78a427501
Lucas Cowe <lucascowe@gmail.com>
2023-10-04 16:43:59 -0500

    Create card.py

---
+++ card.py
@@ -0,0 +1,11 @@
+class Card:
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+    def get_value(self):
+        if self.value in ["J", "Q", "K"]:
+            return 10
+        if self.value == "A":
+            return 11
+        return int(self.value)
