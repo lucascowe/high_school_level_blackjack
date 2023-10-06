3767290685c72208df6a31c39afba9a13fb5f972
Lucas Cowe <lucas@hyperia.net>
2023-10-06 10:20:46 -0600

    more test stuff

---
+++ client/test_blackjack.py
@@ -0,0 +1,52 @@
+import pytest
+from flask_testing import TestCase
+from app import app, deck, player_hand, dealer_hand
+
+class FlaskAppTest(TestCase):
+
+    def create_app(self):
+        app.config['TESTING'] = True
+        return app
+
+    def setUp(self):
+        # Executed before every test
+        self.deck = deck
+        self.deck.shuffle()
+        self.player_hand = player_hand
+        self.dealer_hand = dealer_hand
+
+    def tearDown(self):
+        # Executed after every test
+        pass
+
+    def test_start_game(self):
+        response = self.client.get('/start')
+        json_response = response.get_json()
+        self.assert200(response)
+        assert 'player' in json_response
+        assert 'dealer' in json_response
+        assert len(json_response['player']) == 2
+        assert len(json_response['dealer']) == 2
+
+    def test_hit(self):
+        # Start a game first
+        self.client.get('/start')
+        response = self.client.get('/hit')
+        json_response = response.get_json()
+        self.assert200(response)
+        assert 'player' in json_response
+        assert len(json_response['player']) >= 3  # Because we've hit once
+
+    def test_stand(self):
+        # Start a game first
+        self.client.get('/start')
+        response = self.client.get('/stand')
+        json_response = response.get_json()
+        self.assert200(response)
+        assert 'dealer' in json_response
+        assert 'status' in json_response
+
+# More tests can be added similarly
+
+if __name__ == '__main__':
+    pytest.main()
